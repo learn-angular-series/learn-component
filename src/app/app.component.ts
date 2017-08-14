dc7552f52f5c092da8271fa3d15ef2f26e98db49
zhang.xiaofei <damoqiongqiu@126.com>
2017-08-14 12:54:49 +0800

    更新内容

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '星际争霸2';
+  public title = '星际争霸2';
+
+  public isShow:boolean=true;
 
   public getVal():any{
     return 65535;
+  }
+
+  public sayHello(name:string):void{
+    alert(name);
+  }
+
+  public toggleShow():void{
+    this.isShow=!this.isShow;
   }
 }
