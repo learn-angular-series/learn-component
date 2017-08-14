a6cd235fad0bef5bb82c880122ac76b875a165a6
zhang.xiaofei <damoqiongqiu@126.com>
2017-08-14 13:06:49 +0800

    更新内容

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = '星际争霸2';
+  public title = '假的星际争霸2';
 
   public isShow:boolean=true;
+
+  public races:Array<any>=[
+    {name:"人族"},
+    {name:"虫族"},
+    {name:"神族"}
+  ];
+
+  public mapStatus:string="下载中";
 
   public getVal():any{
     return 65535;
