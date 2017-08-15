b7cb9f0bfc1eef37fd95892bb5133d15b9af3e3a
zhang.xiaofei <damoqiongqiu@126.com>
2017-08-15 12:08:42 +0800

    更新注释

--- src/app/life-cycle/life-cycle.component.ts
+++ src/app/life-cycle/life-cycle.component.ts
@@ -11,7 +11,12 @@ export class LifeCycleComponent implements OnInit {
   @Input()
   public title:string="";
 
-  constructor() { 
+  constructor() {
+    /**
+     * 构造函数里面最好只做一些简单的赋值操作，不要做复杂的逻辑
+     * 获取服务端数据、业务逻辑等不要实现在构造函数里面
+     * 想知道详细的原因请看这篇解释http://misko.hevery.com/code-reviewers-guide/flaw-constructor-does-real-work/
+     */
     this.logs.push("constructor...");
   }
 
