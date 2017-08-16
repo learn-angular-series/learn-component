f20873d5610c10db138eb5342eed5748758346b5
zhang.xiaofei <damoqiongqiu@126.com>
2017-08-16 19:35:41 +0800

    修改注释

--- src/app/dynamic-comp/dynamic-comp.component.ts
+++ src/app/dynamic-comp/dynamic-comp.component.ts
@@ -15,7 +15,9 @@ export class DynamicCompComponent implements OnInit {
   comp1:ComponentRef<Child11Component>;
   comp2:ComponentRef<Child11Component>;
 
-  constructor(private resolver:ComponentFactoryResolver) {
+  constructor(
+    private resolver:ComponentFactoryResolver) {
+
   }
 
   ngOnInit() {
@@ -24,21 +26,20 @@ export class DynamicCompComponent implements OnInit {
   ngAfterContentInit(){
     console.log("动态创建组件的实例...");
     const childComp=this.resolver.resolveComponentFactory(Child11Component);
-    /**
-     * createComponent方法可以调用很多次，会动态创建出多个组件实例
-     * 方法有第二个参数，表示组件渲染的顺序
-     */
     this.comp1=this.dyncomp.createComponent(childComp); 
     this.comp1.instance.title="设置一个新的标题";
     this.comp1.instance.btnClick.subscribe((param)=>{
       console.log(param);
     });
-
+    
+    /**
+     * createComponent方法可以调用很多次，会动态创建出多个组件实例
+     * 方法有第二个参数，表示组件渲染的顺序
+     */
     this.comp2=this.dyncomp.createComponent(childComp,0); 
   }
 
   public destoryChild():void{
-    //this.dyncomp.clear();
     this.comp1.destroy();
   }
 }
