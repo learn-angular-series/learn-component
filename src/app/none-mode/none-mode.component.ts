30d325a41bf66948a8785fd0ac8c0d3f578c0fd8
damoqiongqiu <damoqiongqiu@126.com>
2018-06-30 15:38:08 +0800

    update

--- src/app/none-mode/none-mode.component.ts
+++ src/app/none-mode/none-mode.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'none-mode',
-  encapsulation:ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './none-mode.component.html',
   styleUrls: ['./none-mode.component.scss']
 })
