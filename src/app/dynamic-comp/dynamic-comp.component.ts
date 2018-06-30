a503ad433af86ffd148cccb95e0d169fe2461f2c
damoqiongqiu <damoqiongqiu@126.com>
2018-06-30 15:14:27 +0800

    update

--- src/app/dynamic-comp/dynamic-comp.component.ts
+++ src/app/dynamic-comp/dynamic-comp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild,ViewContainerRef,ComponentFactoryResolver, ComponentRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentRef } from '@angular/core';
 import { Child11Component } from './child11/child11.component';
 import { state } from '@angular/animations';
 
@@ -9,51 +9,50 @@ import { state } from '@angular/animations';
 })
 export class DynamicCompComponent implements OnInit {
   //这里引用模板里面定义的dyncomp容器标签
-  @ViewChild("dyncomp",{read:ViewContainerRef})
-  dyncomp:ViewContainerRef;
+  @ViewChild("dyncomp", { read: ViewContainerRef })
+  dyncomp: ViewContainerRef;
 
-  comp1:ComponentRef<Child11Component>;
-  comp2:ComponentRef<Child11Component>;
+  comp1: ComponentRef<Child11Component>;
+  comp2: ComponentRef<Child11Component>;
 
   constructor(
-    private resolver:ComponentFactoryResolver) {
+    private resolver: ComponentFactoryResolver) {
 
   }
 
   ngOnInit() {
   }
 
-  ngAfterContentInit(){
+  ngAfterContentInit() {
     console.log("动态创建组件的实例...");
-    const childComp=this.resolver.resolveComponentFactory(Child11Component);
-    this.comp1=this.dyncomp.createComponent(childComp); 
-    // this.comp1.instance.title="父层设置的新标题";
-    
-    // this.comp1.instance.btnClick.subscribe((param)=>{
-    //   console.log("--->"+param);
-    // });
+    const childComp = this.resolver.resolveComponentFactory(Child11Component);
+    this.comp1 = this.dyncomp.createComponent(childComp);
+    this.comp1.instance.title = "111";
+    this.comp1.instance.btnClick.subscribe((param) => {
+      console.log("--->" + param);
+    });
 
     //可以创建多个组件实例出来
-    // let temp1=this.dyncomp.createComponent(childComp); 
-    // temp1.instance.title="第2个动态子组件";
-    // let temp2=this.dyncomp.createComponent(childComp); 
-    // temp2.instance.title="第3个动态子组件";
-    // let temp3=this.dyncomp.createComponent(childComp); 
-    // temp3.instance.title="第4个动态子组件";
-    // let temp4=this.dyncomp.createComponent(childComp); 
-    // temp4.instance.title="第5个动态子组件";
-    // let temp5=this.dyncomp.createComponent(childComp);
-    // temp5.instance.title="第6个动态子组件"; 
-    
+    // let temp1 = this.dyncomp.createComponent(childComp);
+    // temp1.instance.title = "第2个动态子组件";
+    // let temp2 = this.dyncomp.createComponent(childComp);
+    // temp2.instance.title = "第3个动态子组件";
+    // let temp3 = this.dyncomp.createComponent(childComp);
+    // temp3.instance.title = "第4个动态子组件";
+    // let temp4 = this.dyncomp.createComponent(childComp);
+    // temp4.instance.title = "第5个动态子组件";
+    // let temp5 = this.dyncomp.createComponent(childComp, 0);
+    // temp5.instance.title = "第6个动态子组件";
+
     /**
      * createComponent方法可以调用很多次，会动态创建出多个组件实例
      * 方法有第二个参数，表示组件渲染的顺序
      */
-    this.comp2=this.dyncomp.createComponent(childComp,0);
-    this.comp2.instance.title="第二个子组件";
+    this.comp2 = this.dyncomp.createComponent(childComp);
+    this.comp2.instance.title = "第二个子组件";
   }
 
-  public destoryChild():void{
+  public destoryChild(): void {
     this.comp1.destroy();
     this.comp2.destroy();
   }
