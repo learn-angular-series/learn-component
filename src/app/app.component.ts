a8f750c6db9ede028ff2725887f812c8dcaad3dd
damoqiongqiu <damoqiongqiu@126.com>
2019-06-22 22:56:50 +0800

    生命周期钩子里面不可以修改属性的情况

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,25 @@ export class AppComponent {
       this.myTitle = "新标题" + new Date().toLocaleString();
     }, 1000);
   }
+
+  ngOnChanges() {
+    this.myTitle = "新标题" + new Date().toLocaleString();
+  }
+
+  ngDoCheck(){
+    // this.myTitle = "新标题" + new Date().toLocaleString();
+  }
+
+  ngAfterContentChecked(){
+    // this.myTitle = "新标题" + new Date().toLocaleString();
+  }
+
+  /**
+   * Note：这个地方会抛异常
+   * 因为ngAfterViewChecked是最后一个钩子，执行这个钩子的时候已经完成了上一轮detection
+   * 所以这个时候，不可以再修改组件上用来进行值绑定的那些属性了，否则就会抛出异常
+   */
+  ngAfterViewChecked() {
+    // this.myTitle = "新标题" + new Date().toLocaleString();
+  }
 }
