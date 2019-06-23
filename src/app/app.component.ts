81439467abedd8576f7837e215e7f285be07592e
damoqiongqiu <damoqiongqiu@126.com>
2019-06-23 08:00:21 +0800

    添加一些注释

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public myTitle: string = "默认标题";
   
+  /**
+   * 这个例子用来解释changeDetection的执行次数
+   * 详细的解释在这里：https://blog.angularindepth.com/a-gentle-introduction-into-change-detection-in-angular-33f9ffff6f10
+   */
+  // public _myTitle: string = "默认标题";
+  // public get myTitle(){
+  //   this._myTitle = "新标题" + new Date().toLocaleString();
+  //   return this._myTitle;
+  // }
+
   constructor() {
+    //这里可以看到，定时器可以触发detection，这是由zone.js来保证的
     setInterval(() => {
       this.myTitle = "新标题" + new Date().toLocaleString();
     }, 1000);
+  }
+
+  /**
+   * 与constructor的区别有两点：
+   * - constructor里面可以进行依赖注入，ngOnInit不可以
+   * - 对于@Input型的属性，在constructor里面访问是没有意义的，因为@Input型的属性与脏检查机制有关，这些属性到ngOnInit的时候才会有值（业务开发要特别注意这一点）
+   * - 在业务开发过程中，ngOnInit用来放一些初始逻辑
+   */
+  ngOnInit(){
+    //业务开发要特别注意，@Input型的属性到这里才会有值
   }
 
   ngOnChanges() {
