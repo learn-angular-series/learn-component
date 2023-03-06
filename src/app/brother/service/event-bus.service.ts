1f2d7636e118f342443acfac51dcb83c3b8b1805
大漠穷秋 <damoqiongqiu@126.com>
2023-03-06 20:07:46 +0800

    upgrade-to-v15

--- src/app/brother/service/event-bus.service.ts
+++ src/app/brother/service/event-bus.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 
 /**
  * 用来充当事件总线的Service
