fb53a00e8e0d2d6f12504b0e9e70089acb7f0336
大漠穷秋 <damoqiongqiu@126.com>
2023-03-06 19:51:33 +0800

    upgrade-to-v15

--- src/app/shadow-dom-mode/shadow-dom-mode.component.ts
+++ src/app/shadow-dom-mode/shadow-dom-mode.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'shadow-dom-mode',
-  encapsulation:ViewEncapsulation.Native,
+  encapsulation:ViewEncapsulation.ShadowDom,
   templateUrl: './shadow-dom-mode.component.html',
   styleUrls: ['./shadow-dom-mode.component.scss']
 })
