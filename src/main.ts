bd832ac3d95a5f637490840fee36c82932b33f6a
大漠穷秋 <damoqiongqiu@126.com>
2023-03-20 22:07:44 +0800

    add comments

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,10 @@ if (environment.production) {
   enableProdMode();
 }
 
+/**
+ * 直接从独立组件启动应用，彻底摆脱 NgModule 机制。
+ * 事实上，从 Angular2 发布以来，社区对 NgModule 机制就有非常多的争议，NgModule 是一个典型的过度设计。
+ * NgModule 机制引入了额外的复杂性，这个机制在其它任何前端框架中都没有出现过。
+ * 参见：https://angular.io/guide/standalone-components
+ */
 bootstrapApplication(StandaloneComponentComponent);
